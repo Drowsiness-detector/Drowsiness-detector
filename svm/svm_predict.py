23043987b4d42552ed2439a21e77f69d48e11038
brisk286 <1459588761@qq.com>
2021-02-03 22:22:04 +0800

    2.3-1

--- svm/svm_predict.py
+++ svm/svm_predict.py
@@ -1,7 +1,7 @@
 import pickle
 
 def svm_predict(eyes ,mouth):
-    with open(r'C:\Users\14595\Desktop\docker\svm\svm_model_7.pickle','rb') as f:
+    with open('svm/svm_model_7.pickle','rb') as f:
         model = pickle.load(f)
         pred = model.predict([[eyes, mouth]])
         return pred
